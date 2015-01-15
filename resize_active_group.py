7c1f69176591917ecfea3eaf3d8c27ed46bc02e4
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-01-15 01:05:05 +0100

    Added a settings-file that can be used to modify the resize-delta for the groups

--- resize_active_group.py
+++ resize_active_group.py
@@ -1,5 +1,28 @@
 import sublime, sublime_plugin
 
+settings_file = "resize_active_group.sublime-settings"
+settings = None
+
+# Loads the settings specified by 'settings_file' and adds a callback to be called every time
+# the 'settings_file' changes.
+def plugin_loaded():
+	global settings
+
+	settings = sublime.load_settings(settings_file)
+	settings.add_on_change(settings_file, load_deltas)
+	load_deltas()
+
+# Loads the four different delta values used by the 'ResizeActiveGroupCommand'.
+# It is called by 'plugin_loaded' and every time the 'settings_file' changes.
+def load_deltas():
+	global delta_up, delta_down, delta_left, delta_right
+
+	delta_default = settings.get("delta_default", 0.1)
+	delta_up = settings.get("delta_up", delta_default)
+	delta_down = settings.get("delta_down", delta_default)
+	delta_left = settings.get("delta_left", delta_default)
+	delta_right = settings.get("delta_right", delta_default)
+
 XMIN, YMIN, XMAX, YMAX = range(4)
 
 class ResizeActiveGroupCommand(sublime_plugin.WindowCommand):
@@ -22,19 +45,19 @@ class ResizeActiveGroupCommand(sublime_plugin.WindowCommand):
 		if direction == 'up':
 			items = rows
 			index = cell[YMAX]
-			delta = -0.01
+			delta = -delta_up
 		if direction == 'down':
 			items = rows
 			index = cell[YMAX]
-			delta = 0.01
+			delta = delta_down
 		if direction == 'left':
 			items = cols
 			index = cell[XMAX]
-			delta = -0.01
+			delta = -delta_left
 		if direction == 'right':
 			items = cols
 			index = cell[XMAX]
-			delta = 0.01
+			delta = delta_right
 
 		if len(items) == 2:
 			return
@@ -46,3 +69,8 @@ class ResizeActiveGroupCommand(sublime_plugin.WindowCommand):
 
 		layout =  {"cols": cols, "rows": rows, "cells": cells}
 		self.window.set_layout(layout)
+
+# The 'plugin_loaded' is automatically called by ST3 when the API is ready.
+# If we're using ST2, it has to be called explicitly.
+if not int(sublime.version()) > 3000:
+	plugin_loaded()
