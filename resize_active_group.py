d80c91bb160fc36a31ddb462475ce93fd813207d
Vadim Pashkov <Vadim_P@abbyy.com>
2018-09-05 18:06:26 +0300

    Add paddings between splitter and window border #4

--- resize_active_group.py
+++ resize_active_group.py
@@ -16,12 +16,21 @@ def plugin_loaded():
 # It is called by 'plugin_loaded' and every time the 'settings_file' changes.
 def load_deltas():
 	global delta_up, delta_down, delta_left, delta_right
+	global padding_up, padding_down, padding_left, padding_right
 
 	delta_default = settings.get("resize_delta_default", 0.01)
 	delta_up = settings.get("resize_delta_up", delta_default)
 	delta_down = settings.get("resize_delta_down", delta_default)
 	delta_left = settings.get("resize_delta_left", delta_default)
 	delta_right = settings.get("resize_delta_right", delta_default)
+
+	vertical_padding_default = settings.get("resize_vertical_padding_default", 0.05)
+	horizontal_padding_default = settings.get("resize_horizontal_padding_default", 0.03)
+
+	padding_up = settings.get("resize_padding_up", vertical_padding_default)
+	padding_down = settings.get("resize_padding_down", vertical_padding_default)
+	padding_left = settings.get("resize_padding_left", horizontal_padding_default)
+	padding_right = settings.get("resize_padding_right", horizontal_padding_default)
 
 XMIN, YMIN, XMAX, YMAX = range(4)
 
@@ -46,18 +55,25 @@ class ResizeActiveGroupCommand(sublime_plugin.WindowCommand):
 			items = rows
 			index = cell[YMAX]
 			delta = -delta_up
+			padding = padding_up
+
 		if direction == 'down':
 			items = rows
 			index = cell[YMAX]
 			delta = delta_down
+			padding = padding_down
+
 		if direction == 'left':
 			items = cols
 			index = cell[XMAX]
 			delta = -delta_left
+			padding = padding_left
+
 		if direction == 'right':
 			items = cols
 			index = cell[XMAX]
 			delta = delta_right
+			padding = padding_right
 
 		if len(items) == 2:
 			return
@@ -65,7 +81,12 @@ class ResizeActiveGroupCommand(sublime_plugin.WindowCommand):
 		if index == len(items) - 1:
 			index -= 1
 
-		items[index] += delta
+		min_value = 0 + padding
+		max_value = 1 - padding
+		new_value = round(items[index] + delta, 2)
+		new_value = max(new_value, min_value)
+		new_value = min(new_value, max_value)
+		items[index] = new_value
 
 		layout =  {"cols": cols, "rows": rows, "cells": cells}
 		self.window.set_layout(layout)
